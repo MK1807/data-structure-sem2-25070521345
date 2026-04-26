9d46fbfe1ac8623977f0848b006c33077858d179
MOHIT <mohitkhandal25@gmail.com>
2026-04-26 15:54:57 +0530

    Add files via upload

---
+++ value_added_problem_1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+ int data;
+ struct Node *left;
+ struct Node *right;
+};
+struct Node* createNode(int data) {
+ struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+ if (newNode == NULL) exit(1);
+ newNode->data = data;
+ newNode->left = newNode->right = NULL;
+ return newNode;
+}
+struct Node* insert(struct Node* root, int data) {
+ if (root == NULL) return createNode(data);
+
+ if (data < root->data) {
+ root->left = insert(root->left, data);
+ } else {
+ root->right = insert(root->right, data);
+ }
+ return root;
+}
+void findInRange(struct Node* root, int low, int high) {
+ if (root == NULL) return;
+
+ if (low < root->data)
+ findInRange(root->left, low, high);
+
+ if (root->data >= low && root->data <= high)
+ printf("%d ", root->data);
+
+ if (high > root->data)
+ findInRange(root->right, low, high);
+}
+int main() {
+ printf("Name : Mohit Khandal\n");
+ printf("Section : F2\n");
+ printf("PRN : 25070521345\n");
+ struct Node* root = NULL;
+ root = insert(root, 17);
+ root = insert(root, 15);
+ root = insert(root, 4);
+ root = insert(root, 18);
+
+ printf("Output: ");
+ findInRange(root, 4, 18);
+ printf("\n");
+ return 0;
+}
---
+++ value_added_problem_2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+char stack[MAX];
+int top = -1;
+void push(char c) {
+ stack[++top] = c;
+}
+char pop() {
+ if (top == -1) return '\0';
+ return stack[top--];
+}
+int isMatchingPair(char open, char close) {
+ if (open == '(' && close == ')') return 1;
+ if (open == '{' && close == '}') return 1;
+ if (open == '[' && close == ']') return 1;
+ return 0;
+}
+int isBalanced(char* s) {
+ for (int i = 0; s[i] != '\0'; i++) {
+ if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+ push(s[i]);
+ } else if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+ if (top == -1 || !isMatchingPair(pop(), s[i])) {
+ return 0;
+ }
+ }
+ }
+ return (top == -1);
+}
+int main() {
+ printf("Name : Mohit Khandal\n");
+ printf("Section : F2\n");
+ printf("PRN : 25070521345\n");
+ printf("\n");
+
+ char expr[100];
+ printf("Enter expression: ");
+ scanf("%s", expr);
+
+ if (isBalanced(expr))
+ printf("true\n");
+ else
+ printf("false\n");
+ return 0;
+}
